4c6e553d2e53dbef2fdc18a51d5909e19d2ce04c
HenrietteAamand <201905063@post.au.dk>
2021-11-05 10:50:19 +0100

    Lave filereader for HRMpro og forrunner

---
+++ filreader.py
@@ -0,0 +1,28 @@
+class filereader_class:
+    def __init__(self, path, filename) -> None:
+        self.path = path
+        self.filename = filename
+        pass    
+        
+    def read_HRMpro(self, timestamp_begin, timestamp_end, testpersonnummer):
+        hrmpro_path = self.path + "/Testperson_" + str(testpersonnummer) + "/SimulANT+ Logs - HRM-Pro/"
+        filename = "Heart Rate Display ANT Messages.txt"
+        fullpath = hrmpro_path + filename
+        
+        file = open(fullpath, 'r')
+        lines_From_Logfile = file.readlines()
+        return lines_From_Logfile
+
+    def read_forerunner(self, timestamp_begin, timestamp_end, testpersonnummer):
+        forerunner_path = self.path + "/Testperson_" + str(testpersonnummer) + "/SimulANT+ Logs - HRM-Pro/"
+        filename = "Heart Rate Display ANT Messages.txt"
+        fullpath = forerunner_path + filename
+        file = open(fullpath, 'r')
+        lines_From_Logfile = file.readlines()
+        return lines_From_Logfile
+
+    def read_empatica(self, timestamp_begin, timestamp_end):
+        pass
+    
+    def read_maxrefdes(self):
+        pass
---
+++ tidskorrigering.py
@@ -0,0 +1,13 @@
+
+class tidskorrigering_class:
+    def __init__(self) -> None:
+        pass
+
+    def hrm_pro(self):
+        pass
+
+    def forerunner(self):
+        pass
+
+    def empatica(self):
+        pass
