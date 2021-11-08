64824f5567acf6eef7ffa01d5a2aeda3a119fdb6
HenrietteAamand <201905063@post.au.dk>
2021-11-08 09:49:10 +0100

    Filereader read_maxrefdes_raa_observationer implementeret uden test

--- filreader.py
+++ filreader.py
@@ -1,10 +1,21 @@
+import csv, os.path
+
 class filereader_class:
     def __init__(self, path, filename) -> None:
         self.path = path
         self.filename = filename
         pass    
         
-    def read_HRMpro(self, testpersonnummer):
+    def read_HRMpro(self, testpersonnummer: int):
+        """Metoden læser data fra den logfil, der genereres når man bruger SimulANT+ til at læse data der sendes fra en Garmin(R) ANT+ sensor. 
+           Der indlæses en liste, hvor hver plads i listen indeholder en linjje fra logfilen på formen '81722734 : Rx: [00][FF][FF][FF][86][02][9F][38]'
+
+        Args:
+            testpersonnummer (int): nummer på testpersonnumer fra forøget. Bruges til at åbne den rigtige fil tilhørende den rigtige person. 
+
+        Returns:
+            lines_from_logfile (list<string>): Returnerer en liste med strings svarende til hver linje i logfilen
+        """
         hrmpro_path = self.path + "/Testperson_" + str(testpersonnummer) + "/SimulANT+ Logs - HRM-Pro/"
         filename = "Heart Rate Display ANT Messages.txt"
         fullpath = hrmpro_path + filename
@@ -24,5 +35,27 @@ class filereader_class:
     def read_empatica(self, timestamp_begin, timestamp_end):
         pass
     
-    def read_maxrefdes(self):
-        pass
+
+    def read_maxrefdes_Raa_observationer(self, testpersonnummer: int, fasenummer: int):
+        """Metoden læser data ind fra de rå onservationer, hvor der kan udtrækkes både RR, hr, timestamps m.v. Data indlæses som en .csv fil, og indlses inds i et dictionary
+
+        Args:
+            testpersonnummer (int): nummer på testpersonnumer fra forøget. Bruges til at åbne den rigtige fil tilhørende den rigtige person. 
+            fasenummer (int): nummer på fasen, der bruges til at åbne den rigtige fil
+
+        Returns:
+            list<Dict>: Der returneres en liste med dictionaries. Der er følgende entries i dictionariet: timestmp,smpleCnt,grnCnt,led2,led3,grn2Cnt,irCnt,redCnt,accelX,accelY,accelZ,hr,rr,rrsecure,spo2. Timestamp angives som et klokkeslet uden dato 
+        """
+        i = 0
+        faser = ["Stilhed", "Statisk", "Dynamisk"]
+
+        while(i<3):
+            maxrefdes_path = self.path + "/Testperson" + str(testpersonnummer) + "/Raaobservationer/Testperson_" + str(testpersonnummer) + "_Fase_" + str(fasenummer) + "_" + faser[i] + "_maxdata.csv"
+            if(os.path.exists(maxrefdes_path)):
+                i = 3
+                csv_file_original =  open(maxrefdes_path, mode='r')
+                lines_as_dict = csv.DictReader(csv_file_original, delimiter = ',')
+                lines_as_dict = list(lines_as_dict)
+                csv_file_original.close()
+                return lines_as_dict
+    
